853b82dbc12f860f7b51748e06c55b368ccfa1c5
gustavodovale <gust.dovale@gmail.com>
2026-03-30 21:05:45 -0300

    correção do erro da frequencia ('e', 's')  retorna 6.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 # Tarefa 01 - O Motor de Frequências 
 vocabularios = {
  'l o w </w>': 5,
@@ -25,7 +23,7 @@ def get_stats(lista_vocabularios):
             par_atual
             if par_atual not in pares:
                 pares[par_atual] = 0
-                pares[par_atual] += frequencia
+            pares[par_atual] += frequencia
             
     return pares
 
@@ -36,6 +34,9 @@ if __name__ == "__main__":
     for par, contagem in estatisticas.items():
         print(f"{par}: {contagem}")
         
-    print("\nVerificarda funcionalidade da tarefa 1 especificando apenas a frequencia de um par ('l', 'o)")
-    par_teste = ('l', 'o')
+    print("\nVerificarda funcionalidade da tarefa 1 especificando apenas a frequencia de um par ('e', 's')")
+    par_teste = ('e', 's')
     print(f"O par {par_teste} aparece {estatisticas[par_teste]} vezes.")
+
+
+# Tarefa 2: O Loop de Fusão
